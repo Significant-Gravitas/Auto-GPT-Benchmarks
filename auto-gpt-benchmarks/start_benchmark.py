8fab1f2c77466abfae719b32a3c308f4232bfa27
Silen Naihin <silen.naihin@gmail.com>
2023-06-20 18:19:28 -0400

    read me, config selection

--- auto-gpt-benchmarks/start_benchmark.py
+++ auto-gpt-benchmarks/start_benchmark.py
@@ -1,11 +1,34 @@
 import click
 import pytest
+import json
 
 
 @click.command()
 @click.option("--challenge", default=None, help="Specific challenge to run")
 def start(challenge):
     """Start the benchmark tests. If a challenge flag is is provided, run the challenges with that mark."""
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+    print("Current configuration:")
+    for key, value in config.items():
+        print(f"{key}: {value}")
+
+    update_config = click.confirm(
+        "\nDo you want to update these parameters?", default=False
+    )
+    if update_config:
+        config["hostname"] = click.prompt(
+            "\nPlease enter a new hostname", default=config["hostname"]
+        )
+        config["port"] = click.prompt("Please enter a new port", default=config["port"])
+        config["workspace"] = click.prompt(
+            "Please enter a new workspace path", default=config["workspace"]
+        )
+
+        with open("config.json", "w") as f:
+            json.dump(config, f)
+
     if challenge:
         pytest.main(["-m", challenge])
     else:
