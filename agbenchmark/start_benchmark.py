759f92d393c249a3c28c07f31baa1a9fb6a1e3c4
Silen Naihin <silen.naihin@gmail.com>
2023-06-21 20:28:14 -0400

    rename flag to category, config only once

--- agbenchmark/start_benchmark.py
+++ agbenchmark/start_benchmark.py
@@ -10,53 +10,55 @@ def cli():
 
 
 @cli.command()
-@click.option("--challenge", default=None, help="Specific challenge to run")
+@click.option("--category", default=None, help="Specific category to run")
 @click.option("--noreg", is_flag=True, help="Skip regression tests")
-def start(challenge, noreg):
-    """Start the benchmark tests. If a challenge flag is is provided, run the challenges with that mark."""
-    with open("agbenchmark/config.json", "r") as f:
-        config = json.load(f)
+def start(category, noreg):
+    """Start the benchmark tests. If a category flag is is provided, run the categories with that mark."""
+    """Start the benchmark tests. If a category flag is provided, run the categories with that mark."""
+    config_file = "agbenchmark/config.json"
+
+    # Check if configuration file exists and is not empty
+    if not os.path.exists(config_file) or os.stat(config_file).st_size == 0:
+        config = {}
+
+        config["hostname"] = click.prompt(
+            "\nPlease enter a new hostname", default="localhost"
+        )
+        config["port"] = click.prompt("Please enter a new port", default=8080)
+        config["workspace"] = click.prompt(
+            "Please enter a new workspace path", default="/path/to/workspace"
+        )
+
+        with open(config_file, "w") as f:
+            json.dump(config, f)
+    else:
+        # If the configuration file exists and is not empty, load it
+        with open(config_file, "r") as f:
+            config = json.load(f)
 
     print("Current configuration:")
     for key, value in config.items():
         print(f"{key}: {value}")
 
-    update_config = click.confirm(
-        "\nDo you want to update these parameters?", default=False
-    )
-    if update_config:
-        config["hostname"] = click.prompt(
-            "\nPlease enter a new hostname", default=config["hostname"]
-        )
-        config["port"] = click.prompt("Please enter a new port", default=config["port"])
-        config["workspace"] = click.prompt(
-            "Please enter a new workspace path", default=config["workspace"]
-        )
-
-        with open("agbenchmark/config.json", "w") as f:
-            json.dump(config, f)
-
-    print("Starting benchmark tests...", challenge)
+    print("Starting benchmark tests...", category)
     pytest_args = ["agbenchmark", "-vs"]
-    if challenge:
+    if category:
         pytest_args.extend(
-            ["-m", challenge]
-        )  # run challenges that are of a specific marker
+            ["-m", category]
+        )  # run categorys that are of a specific marker
         if noreg:
             pytest_args.extend(
                 ["-k", "not regression"]
-            )  # run challenges that are of a specific marker but don't include regression challenges
-        print(
-            f"Running {'non-regression' + challenge if noreg else challenge} challenges"
-        )
+            )  # run categorys that are of a specific marker but don't include regression categorys
+        print(f"Running {'non-regression' + category if noreg else category} categorys")
     else:
         if noreg:
-            print("Running all non-regression challenges")
+            print("Running all non-regression categorys")
             pytest_args.extend(
                 ["-k", "not regression"]
-            )  # run challenges that are not regression challenges
+            )  # run categorys that are not regression categorys
         else:
-            print("Running all challenges")  # run all challenges
+            print("Running all categorys")  # run all categorys
 
     # Run pytest with the constructed arguments
     pytest.main(pytest_args)
