cc6467d62921332a8b76c9c716b1b329a1c189a0
Silen Naihin <silen.naihin@gmail.com>
2023-07-31 16:13:01 +0100

    internal info, challenges path

--- agbenchmark/reports/ReportManager.py
+++ agbenchmark/reports/ReportManager.py
@@ -42,18 +42,8 @@ class ReportManager:
         with open(self.filename, "w") as f:
             json.dump(self.tests, f, indent=4)
 
-    def add_test(
-        self,
-        test_name: str,
-        test_details: dict | list,
-        agent_name: Optional[str] = None,
-    ) -> None:
-        if agent_name:
-            if agent_name not in self.tests:
-                self.tests[agent_name] = {}
-            self.tests[agent_name][test_name] = test_details
-        else:
-            self.tests[test_name] = test_details
+    def add_test(self, test_name: str, test_details: dict | list) -> None:
+        self.tests[test_name] = test_details
 
         self.save()
 
